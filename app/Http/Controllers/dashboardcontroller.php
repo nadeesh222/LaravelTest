49c452f003ade3b9a6b9e93f768a53f5b116b445
nadeesh <nadeesh@cbsl.lk>
2025-04-03 15:02:16 +0530

    Initial Laravel project commit

---
+++ app/Http/Controllers/dashboardcontroller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Idea;
+
+use Illuminate\Http\Request;
+
+class dashboardcontroller extends Controller
+{
+     public function index(){
+
+        $idea = new Idea(
+            ['content'=>'test','likes'=>10
+
+            ]
+
+        );
+       // $idea->save();
+
+       // dump(Idea::all());
+
+        return view('dashboard',  [ 'ideas'=>Idea::all()] );
+
+     }
+}
