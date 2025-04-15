4add37dfd59c4d827c5f7b0f59cafcedfa64932f
nadeeshtest <nadeesh.t.1@gmail.com>
2025-04-15 09:37:19 +0530

    Tutorial #14 completed delete CRUD

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', [dashboardcontroller::class,'index'])->name('dashboard');
 
-Route::post('/idea', [IdeaController::class,'store'])->name('idea.create');
+Route::post('/ideas', [IdeaController::class,'store'])->name('ideas.store');
+
+Route::delete('/ideas/{id}', [IdeaController::class,'destroy'])->name('ideas.destroy');
 
 
 Route::get('/terms',function(){
